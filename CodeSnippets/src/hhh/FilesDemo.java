23b0d91f198e41236e494cd9b5eb63011e1a1e1e
Harini akula <harini.as690@gmail.com>
2019-05-29 23:44:50 -0400

    Created multiple files in a folder and listed them using listFiles method which returns File[]

--- CodeSnippets/src/hhh/FilesDemo.java
+++ CodeSnippets/src/hhh/FilesDemo.java
@@ -2,6 +2,7 @@ package hhh;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class FilesDemo {
 
@@ -15,12 +16,41 @@ public class FilesDemo {
 		}
 
 		System.out.println("Size of file " + f.getName() + " is " + f.length());
-		System.out.println("Absolute file path: " + f.getAbsolutePath());
 		System.out.println(f.getPath());
 
 		File f2 = new File(
 				"D:\\softwares\\MyGitRepos\\CodeSnippets\\CodeSnippets\\Data\\config.properties");
-		System.out.println(f2.getAbsolutePath());
+		System.out.println("Absolute path of config.properties: "
+				+ f2.getAbsolutePath());
+
+		System.out
+				.println("********************** Printing list of files in a folder ****************************");
+
+		File[] fs = {
+				new File(
+						"D:\\softwares\\MyGitRepos\\CodeSnippets\\CodeSnippets\\Data\\text2.txt"),
+				new File(
+						"D:\\softwares\\MyGitRepos\\CodeSnippets\\CodeSnippets\\Data\\text3.txt"),
+				new File(
+						"D:\\softwares\\MyGitRepos\\CodeSnippets\\CodeSnippets\\Data\\text4.txt") };
+
+		for (File efs : fs) {
+			if (!efs.exists()) {
+				try {
+					efs.createNewFile();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
+		File folderWithFiles = new File(
+				"D:\\softwares\\MyGitRepos\\CodeSnippets\\CodeSnippets\\Data");
+		File[] fa = folderWithFiles.listFiles();
+		for (File ef : fa) {
+			System.out.println(ef.getName());
+		}
 
 	}
 
